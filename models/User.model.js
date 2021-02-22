1d691f627c6da8691a576b2de396719e20ead572
Benjamin megret <benji@MacBook-Pro-de-Benjamin.local>
2021-02-22 09:40:01 +0100

    final version

--- models/User.model.js
+++ models/User.model.js
@@ -5,28 +5,6 @@ const {
 
 
 const userSchema = new Schema({
-<<<<<<< HEAD
-  username: {
-    type: String,
-    unique: true,
-    require: true
-  },
-   email: {
-    type: String,
-    require: true
-  },
-  password: {
-    type: String,
-    require: true
-  },
-  mylist: [String],
-  groups: [String],
-  isAdmin: Boolean,
-  profilePic: {
-    type: String,
-  },
-  bio: String,
-=======
     username: {
         type: String,
         unique: true,
@@ -47,7 +25,6 @@ const userSchema = new Schema({
         type: String,
     },
     bio: String,
->>>>>>> 3c60cf7567d41942af42d762da56761514bcc0fe
 });
 
 const UserModel = model("users", userSchema);
